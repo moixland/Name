c6ff6dd8d38e8c7b6b4d846ef43cf231100617a6
Moix <moixd@asmsa.org>
2019-05-23 12:40:41 -0500

    Added a Platypus. Because reasons.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
   public static void main(String [] args){
     System.out.println("Hellooooooo!");
     System.out.println("I hope this goes well.");
+    System.out.println("Platypus.");
   }
 }
